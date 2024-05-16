62930fd08da5737c8fde02b148b1c1589ed4e883
zacharybaca <zachary.baca@msn.com>
2024-05-16 06:10:14 -0500

    added more methods to class and start game function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Player {
     }
 
     setName(namePicked) {
-        namePicked = readline.question('Please Enter Mario or Luigi as Your Name: ').toLowerCase();
+        /* If User Doesn't Pick Mario or Luigi as Their Name, They Will Get Prompted to Enter a Name Again */
         if (namePicked === "mario" || namePicked === "luigi") {
             this.name = namePicked;
         }
@@ -21,4 +21,51 @@ class Player {
             this.setName(namePicked);
         }
     }
+
+    gotHit() {
+        /* Each Time a Player Gets Hit, Their Status Drops */
+        if (this.status === "Powered Up") {
+            this.status = "Big";
+        }
+        else if (this.status === "Big") {
+            this.status = "Small";
+        }
+        else {
+            this.status = "Dead";
+        }
+    }
+
+    gotPowerUp() {
+        /* When a Player Receives a Power Up, Their Status Will Go Up Accordingly */
+        if (this.status === "Small") {
+            this.status = "Big";
+        }
+        else if (this.status === "Big") {
+            this.status = "Powered Up";
+        }
+        else {
+            this.hasStar = true;
+        }
+    }
+
+    addCoin() {
+        this.totalCoins += 1;
+    }
+
+    print() {
+        if (this.status === "Dead") {
+            console.log(`Name: ${this.name} \n Status: ${this.status} \n Total Coins After Death: ${this.totalCoins}`)
+        }
+        else if (this.hasStar === true) {
+            console.log(`Name: ${this.name} \n Status: ${this.status} \n Total Coins: ${this.totalCoins} \n You Have a Star!`)
+        }
+        else {
+            console.log(`Name: ${this.name} \n Status: ${this.status} \n Total Coins: ${this.totalCoins}`)
+        }
+    }
+}
+
+const startGame = (namePicked) => {
+    namePicked = readline.question('Please Enter Mario or Luigi as Your Name: ').toLowerCase();
+    let player = new Player(namePicked, totalCoins = 0, level = "Small", hasStar = false);
 }
